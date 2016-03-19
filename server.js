877c516a8739004be27ffc043a7f27e771062f2d
Mathew Trivett <matt.trivett@gmail.com>
2016-03-19 18:55:00 +0000

    Got posting messages up and running

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var Pusher = require('pusher');
+var path = require('path');
 var mongoose = require('mongoose');
 var Message = require('./models/Message');
 var faker = require('faker');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://admin:admin@ds037165.mlab.com:37165/intheloop');
 
 var app = express();
+
+// Set the views engine to ejs and the default views directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Use body parser to get data from the user.
+app.use(bodyParser());
 
 var pusher = new Pusher({
     appId: '189125',
@@ -42,6 +51,25 @@ app.get('/messages', function(req,res) {
 		console.log("Found a whole bunch of messages.")
 		res.send({ "messages" : messages });
 	});
+});
+
+app.get('/messages/new', function(req, res) {
+	res.render('messages/new');
+});
+
+app.post('/messages/create', function(req,res) {
+	var message = new Message({
+		user_id: faker.fake('{{name.firstName}} {{name.lastName}}'),
+		body: req.body.message.body,
+		article_id: "Trump trumps a trumpet in trump town.",
+		created_at: Date.now()
+	});
+
+	message.save(function(err, message) {
+		if (err) return console.error(err);
+		console.dir(message);
+		res.redirect('/messages/new');
+	})
 });
 
 app.listen(3000, function() {
